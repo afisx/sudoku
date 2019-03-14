549073a0b84b535fb8ae37f9fb8419d4ced4c9c3
afisx <alexej.moiseenko@gmail.com>
2019-03-14 23:33:52 +0200

    feat: implement function solveSudocu

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+	if (sudokuSolution(matrix, 0, 0)){
+		return matrix;
+	}
+}
+
+function sudokuSolution(matrix, row, col) {
+	var cell = findEmptyCell(matrix, row, col);
+	if (!cell) {
+		return true;
+	}
+	row = cell[0];
+	col = cell[1];
+
+	for (var num = 1; num <= 9; num++) {
+		if (!checkRowColSect(matrix, row, col, num)) {   
+			matrix[row][col] = num;
+			if (sudokuSolution(matrix, row, col) ) {                
+				return true;
+			}  
+			matrix[row][col] = 0;
+		}
+	}
+return false;
+}
+
+function findEmptyCell(matrix, row, col) {
+	for (let i = 0; i < 9; i++){
+		for (let j = 0; j < 9; j++){
+			if (matrix[i][j] == 0) {
+				return [i, j];
+            }	
+		}
+	}
+    return null;
+}
+
+function checkRowColSect(matrix, row, col, num){	
+	for (var i = 0; i < 9; i++){
+		if (matrix[row][i] == num || matrix[i][col] == num){
+			return true;
+		}
+	}
+	let r = Math.floor(row - row%3);
+	let c = Math.floor(col - col%3);
+
+	for (let i = r; i < r+3; i++) {
+		for (let j = c; j < c+3; j++) {
+			if (matrix[i][j] == num) {
+				return true;
+			}
+		}
+	}
+	return false;	
 }
